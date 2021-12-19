b1130512a674680739cbb720402faafd10c68e75
Alexander Rohrig <me@alexanderrohrig.com>
2021-12-19 00:08:51 -0600

    Initial Commit

---
+++ Shared/balangirApp.swift
@@ -0,0 +1,17 @@
+//
+//  balangirApp.swift
+//  Shared
+//
+//  Created by Alexander Rohrig on 12/19/21.
+//
+
+import SwiftUI
+
+@main
+struct balangirApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
